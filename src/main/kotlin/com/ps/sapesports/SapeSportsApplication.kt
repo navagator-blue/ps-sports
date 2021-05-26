9f13f402bb6a7f043603a6819f1b940420180126
Navaneet Agarwal <navaneet.agarwal@gmail.com>
2021-05-26 22:01:37 +0530

    Initial commit

---
+++ src/main/kotlin/com/ps/sapesports/SapeSportsApplication.kt
@@ -0,0 +1,11 @@
+package com.ps.sapesports
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SapeSportsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SapeSportsApplication>(*args)
+}
---
+++ src/test/kotlin/com/ps/sapesports/SapeSportsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ps.sapesports
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SapeSportsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
